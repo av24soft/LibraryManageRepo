c7d36ed5bb54a86c9e294c11535c98d56c52fac7
Nandini malviya <nandinimalviya44@gmail.com>
2024-10-28 16:57:56 +0530

    Add fields for room entity.

--- LibraryManagement/src/main/java/com/libraryManagement/entity/Room.java
+++ LibraryManagement/src/main/java/com/libraryManagement/entity/Room.java
@@ -1,5 +1,33 @@
 package com.libraryManagement.entity;
 
-public class Room {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Room {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+private int roomNo;
+private String roomName;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getRoomNo() {
+	return roomNo;
+}
+public void setRoomNo(int roomNo) {
+	this.roomNo = roomNo;
+}
+public String getRoomName() {
+	return roomName;
+}
+public void setRoomName(String roomName) {
+	this.roomName = roomName;
+}
 }
