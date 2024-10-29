2612970dd64eca83f0d1ccb9d817413ae950557c
ChavanAdi <adichavan3638@gmail.com>
2024-10-29 21:55:06 +0530

    Creating Room api completed

--- LibraryManagement/src/main/java/com/libraryManagement/dto/RoomDto.java
+++ LibraryManagement/src/main/java/com/libraryManagement/dto/RoomDto.java
@@ -2,4 +2,24 @@ package com.libraryManagement.dto;
 
 public class RoomDto {
 
+	private String roomName;
+
+	public String getRoomName() {
+		return roomName;
+	}
+
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+
+	public RoomDto(String roomName) {
+		super();
+		this.roomName = roomName;
+	}
+
+	public RoomDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 }
--- LibraryManagement/src/main/java/com/libraryManagement/serviceImpl/RoomServiceImpl.java
+++ LibraryManagement/src/main/java/com/libraryManagement/serviceImpl/RoomServiceImpl.java
@@ -1,5 +1,23 @@
 package com.libraryManagement.serviceImpl;
 
-public class RoomServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.libraryManagement.dto.RoomDto;
+import com.libraryManagement.entity.Room;
+import com.libraryManagement.repository.RoomRepository;
+import com.libraryManagement.service.RoomService;
+@Service
+public class RoomServiceImpl implements RoomService{
+
+	@Autowired
+	RoomRepository roomRepository;
+	@Override
+	public void createRoom(RoomDto roomDto) {
+		// TODO Auto-generated method stub
+		Room room = new Room();
+		room.setRoomName(roomDto.getRoomName());
+		roomRepository.save(room);
+	}
 
 }
