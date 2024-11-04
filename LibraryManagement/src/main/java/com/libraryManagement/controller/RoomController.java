959c5ad6e732cb23ad3be36e7dba088774f820cc
pneha3132 <pneha3132@gmail.com>
2024-11-04 18:28:53 +0530

    Get Room api completed

--- LibraryManagement/src/main/java/com/libraryManagement/controller/RoomController.java
+++ LibraryManagement/src/main/java/com/libraryManagement/controller/RoomController.java
@@ -1,8 +1,13 @@
 package com.libraryManagement.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +29,19 @@ public class RoomController {
 		roomServiceImpl.createRoom(roomDto);
 		return new ResponseEntity<Room>(HttpStatus.CREATED);
 	}
-}
+	 @GetMapping
+	    public ResponseEntity getAllRooms() {
+	        List<Room> room = roomServiceImpl.getAllRoom();
+	        return new ResponseEntity(room ,HttpStatus.OK);
+	 	   
+	        
+	    }
+
+	    
+	    @GetMapping("/{id}")
+	    public ResponseEntity getRoomById(@PathVariable("id")Integer id) {
+	        return new ResponseEntity(roomServiceImpl.getRoom(id),HttpStatus.OK);
+	   
+	    }
+	}
+
--- LibraryManagement/src/main/java/com/libraryManagement/repository/RoomRepository.java
+++ LibraryManagement/src/main/java/com/libraryManagement/repository/RoomRepository.java
@@ -1,10 +1,14 @@
 package com.libraryManagement.repository;
 
+ 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.libraryManagement.entity.Room;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer>{
+
+	 
 
 }
