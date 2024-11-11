16f4c50648b8cc1b847a433d476b56d4f2d0bfad
Vaibhav Bijwe <vaibhavbijwe30@gmail.com>
2024-11-11 22:09:50 +0530

    Get Vacant Seat Api

--- LibraryManagement/src/main/java/com/libraryManagement/service/SeatService.java
+++ LibraryManagement/src/main/java/com/libraryManagement/service/SeatService.java
@@ -1,11 +1,13 @@
 package com.libraryManagement.service;
 
+import java.util.List;
+
 import com.libraryManagement.dto.SeatDto;
 import com.libraryManagement.entity.Seat;
 
 public interface SeatService {
 	public Seat createSeat(SeatDto dto);
-	
 
+	public List<Seat> getVacantSeats();
 
 }
